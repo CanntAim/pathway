fd22657f683ab0d0107024748e2c020bcaba323b
Pujan Joshi <pujan@guavaworld.com>
2015-10-09 15:37:22 -0400

    codes adjusted in index file also

--- main.js
+++ main.js
@@ -1,6 +1,2 @@
-VQI_pathwayEditor.prototype = new VQI_Observable();
-
-var services = [];
-services['pathwayfinder'] = 'http://137.99.11.36/pathwayVisual/PathwayParser/ajaxJSON.php';
-var VQI_pathwayEditor = new VQI_pathwayEditor("parent",services);
-VQI_pathwayEditor.subscribeTo(VQI_pathwayEditor);
+VQI_PathwayEditor.prototype = new VQI_Observable();
+var objVQI_PathwayEditor = new VQI_PathwayEditor("parent",services);
