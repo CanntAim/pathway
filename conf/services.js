e9585fa6e4024c6f6a354bf8ada8a00eee1bed31
Pujan Joshi <pujan@guavaworld.com>
2016-03-30 13:49:02 -0400

    changed all ajax requests from get to post.

--- conf/services.js
+++ conf/services.js
@@ -3,10 +3,6 @@ if (typeof web_services === 'undefined') {
 }
 /* Pathway Editor */
 web_services['VQI_PATHWAY_EDITOR'] = {};
-web_services['VQI_PATHWAY_EDITOR']["pathwayFinderUrl"] = "http://bibci.engr.uconn.edu/puj07001/web_services/find_path_and_score/find_path_and_score.php";
-web_services['VQI_PATHWAY_EDITOR']['pathwayFinder'] = 'http://cardinal3.engr.uconn.edu/pathwayVisual/PathwayParser/ajaxJSON.php';
-web_services['VQI_PATHWAY_EDITOR']['pathwaySaver'] = 'http://cardinal3.engr.uconn.edu/pathwayVisual/PathwayParser/updateDB_json.php';
-web_services['VQI_PATHWAY_EDITOR']['pathwayScorer'] = 'http://cardinal3.engr.uconn.edu/pathwayVisual/ScoreSystem/getScore.php';
-web_services['VQI_PATHWAY_EDITOR']['pathwayWeightedScorer'] = 'http://137.99.11.122/pathway2/pathwayweightedscorer.php';
-web_services['VQI_PATHWAY_EDITOR']['objectFinder'] = 'http://137.99.11.122/pathway2/qsys_json.php';
-
+web_services['VQI_PATHWAY_EDITOR']["FIND_PATH"] = "http://bibci.engr.uconn.edu/puj07001/web_services/find_path_and_score/find_path_and_score.php";
+web_services['VQI_PATHWAY_EDITOR']['GET_PATHWAY'] = 'http://magellan.engr.uconn.edu/puj07001/web_services/vqi_pathway/get_pathway.php';
+web_services['VQI_PATHWAY_EDITOR']['SAVE_PATHWAY'] = 'http://magellan.engr.uconn.edu/puj07001/web_services/vqi_pathway/save_pathway.php';
