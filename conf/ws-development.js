9a93162a370238c8f7cf341df2d86547cda13f90
Pujan Joshi <pujan.joshi@uconn.edu>
2016-05-16 15:55:08 -0400

    remove 'mode' from ws-development and change reference to ws.js

--- conf/ws-development.js
+++ conf/ws-development.js
@@ -19,6 +19,3 @@ web_services['VQI_PATHWAY_EDITOR'] = {};
 web_services['VQI_PATHWAY_EDITOR']["FIND_PATH"] = "http://magellan.engr.uconn.edu/puj07001/web_services/find_path_and_score/find_path_and_score.php";
 web_services['VQI_PATHWAY_EDITOR']['GET_PATHWAY'] = 'http://magellan.engr.uconn.edu/puj07001/web_services/vqi_pathway/get_pathway.php';
 web_services['VQI_PATHWAY_EDITOR']['SAVE_PATHWAY'] = 'http://magellan.engr.uconn.edu/puj07001/web_services/vqi_pathway/save_pathway.php';
-
-/* Settings */
-var mode = "test";
--- index.php
+++ index.php
@@ -1,33 +1,34 @@
 <html>
-	<head>
-		<meta charset=utf-8/>
-		<link rel="stylesheet" href="style.css"/>
-		<link rel="stylesheet" href="dependencies/jquery-ui-1.11.4/jquery-ui.css"/>
-		<link rel="stylesheet" href="dependencies/bootstrap-3.3.5-dist/css/bootstrap.min.css">
-		<script src="dependencies/download.js"></script>
-		<script src="dependencies/sorttable.js"></script>
-		<script src="dependencies/jquery-1.11.3.min.js"></script>
-		<script src="dependencies/jquery-ui-1.11.4/jquery-ui.js"></script>
-		<script src="dependencies/d3/d3.min.js"></script>
-		<script src="dependencies/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
-		<script src="dependencies/cytoscape.js-2.5.4/cytoscape.js"></script>
-	</head>
-	<body>
+    <head>
+        <meta charset=utf-8/>
+        <link rel="stylesheet" href="style.css"/>
+        <link rel="stylesheet" href="dependencies/jquery-ui-1.11.4/jquery-ui.css"/>
+        <link rel="stylesheet" href="dependencies/bootstrap-3.3.5-dist/css/bootstrap.min.css">
+        <script src="dependencies/download.js"></script>
+        <script src="dependencies/sorttable.js"></script>
+        <script src="dependencies/jquery-1.11.3.min.js"></script>
+        <script src="dependencies/jquery-ui-1.11.4/jquery-ui.js"></script>
+        <script src="dependencies/d3/d3.min.js"></script>
+        <script src="dependencies/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
+        <script src="dependencies/cytoscape.js-2.5.4/cytoscape.js"></script>
+    </head>
+    <body>
 
-		<div id="parent"></div>
-		<script src="conf/ws-development.js"></script>
-		<script src="VQI_PathwayEditor.js"></script>
-		<script src="VQI_PathwayEditorTester.js"></script>
-		<script>
-			if (mode != "test") {
-				var objVQI_PathwayEditor = new VQI_PathwayEditor();
-				objVQI_PathwayEditor.GUI("parent");
-				objVQI_PathwayEditor.NoGUI();
-			} else {
-				var objVQI_PathwayEditorTester = new VQI_PathwayEditorTester();
-				objVQI_PathwayEditorTester.GUI();
-				objVQI_PathwayEditorTester.GUI.runTests();
-			}
-		</script>
-	</body>
+        <div id="parent"></div>
+        <script src="conf/ws.js"></script>
+        <script src="VQI_PathwayEditor.js"></script>
+        <script src="VQI_PathwayEditorTester.js"></script>
+        <script>
+            var mode = "test";
+            if (mode != "test") {
+                var objVQI_PathwayEditor = new VQI_PathwayEditor();
+                objVQI_PathwayEditor.GUI("parent");
+                objVQI_PathwayEditor.NoGUI();
+            } else {
+                var objVQI_PathwayEditorTester = new VQI_PathwayEditorTester();
+                objVQI_PathwayEditorTester.GUI();
+                objVQI_PathwayEditorTester.GUI.runTests();
+            }
+        </script>
+    </body>
 </html>
